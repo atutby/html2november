01183d1db55776c42d9ea26e5dbc54997d490273
e.o.svetashova@gmail.com <sasha2015to@gmail.com>
2019-11-26 17:12:55 +0300

    button.addEventListener preventDefault()

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ let cards = document.querySelectorAll(".portfolio__item");
 cards.forEach(item => (item.style.cssText = `
 border: 3px blue solid; border-radius: 10px; padding: 5px;
 `))
+
+
+button.addEventListener("click", (event) => {
+  event.preventDefault();
+  // event.stopPropagation();
+  confirm('Вам уже исполнилось 18 лет?');
+})
